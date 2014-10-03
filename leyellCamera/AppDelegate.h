9d74d5421bf32d7be392828e5817bb73df86bd6d
work4blue <work4blue@sina.com>
2014-10-04 00:18:56 +0800

    Initial Commit

---
+++ leyellCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  leyellCamera
+//
+//  Created by  Andrew Huang on 14-10-4.
+//  Copyright (c) 2014年  Andrew Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ leyellCamera/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  leyellCamera
+//
+//  Created by  Andrew Huang on 14-10-4.
+//  Copyright (c) 2014年  Andrew Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
